b5847abc43f72ffde45dc8078432499f14c65ff4
oishi junji <abdaqoopmhm@gmail.com>
2023-03-20 15:26:09 +0900

    my_portfolio
    横スクロールを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Header } from './component/Header'
 import './App.css'
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import { Home } from './component/Home'
 import { About } from './component/About'
 import { Skills } from './component/Skills'
@@ -8,14 +8,38 @@ import { Works } from './component/Works'
 import { Contact } from './component/Contact'
 
 const App: FC = () => {
+
+  const screenRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        screenRef.current!.onwheel = event => {
+          console.log(event)
+          event.preventDefault()
+
+            let delta = (event.deltaY / Math.abs(event.deltaY)) * window.innerWidth
+            if (delta > 0) {
+                delta += screenRef.current!.scrollLeft
+                delta = Math.floor(delta / window.innerWidth) * window.innerWidth
+            } else {
+                delta += screenRef.current!.scrollLeft
+                delta = Math.ceil(delta / window.innerWidth) * window.innerWidth
+            }
+            screenRef.current!.scrollLeft = delta
+        }
+    }, [])
+
   return (
     <>
-      <Header />
-      <Home />
-      <About />
-      <Skills />
-      <Works />
-      <Contact />
+      <div ref={screenRef} className='screen'>
+        <div className='container'>
+          <Header />
+          <Home />
+          <About />
+          <Skills />
+          <Works />
+          <Contact />
+        </div>
+      </div>
     </>
   )
 }
