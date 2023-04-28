c2ca02d39a09e1b37da02c5b3f7287fa5de35f04
oishi junji <abdaqoopmhm@gmail.com>
2023-04-28 09:45:06 +0900

    my_portfolio
    - chart.jsを追加

--- src/component/Skills.tsx
+++ src/component/Skills.tsx
@@ -5,19 +5,76 @@ import { SlideScaleChange } from '../utils/SlideScaleChange';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { common } from '../styles/common';
+import { Graph } from '../hooks/Chart';
 
 const skillsContainer = css`
     width: 100%;
     height: 100%;
     background-color: coral;
 `
+const Scale = {
+    y: {
+      min: 0,
+      max: 100,
+      title: {
+        display: true,
+        text: "weight",
+        color: "#FF4500",
+        rotate: "vertical",
+        font: {
+          size: 20,
+        },
+      },
+      ticks: {
+        stepSize: 10,
+      },
+    },
+    x: {
+      title: {
+        display: true,
+        text: "month",
+        color: "rgb(255,69,0)",
+        font: {
+          size: 20,
+        },
+      },
+    },
+  };
+
+const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "graph",
+      },
+    },
+    scales: Scale,
+  };
+  
+  const data = {
+    labels: ["January", "February", "March"],
+    datasets: [
+      {
+        label: "Aさん",
+        data: [55, 50, 60],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "yellow",
+      },
+    ],
+  };
 
 export const Skills = forwardRef<HTMLDivElement, ContainerProps>(({ id, isIntersecting } , ref) => {
     return(
         <div id={id} ref={ref} css={common.wrapper}>
             <SlideScaleChange isIntersecting={isIntersecting}>
                 <div css={skillsContainer}>
-                    <div className='contentWrapper'></div>
+                    <div className='contentWrapper'>
+                        <Graph data={data} options={options} />
+                    </div>
                 </div>
             </SlideScaleChange>
         </div>
---
+++ src/hooks/Chart.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type ChartProps = {
+    data: any;
+    options: any;
+};
+
+export const Graph: React.FC<ChartProps> = ({ data, options }) => {
+    return <Line options={options} data={data} />;
+};
