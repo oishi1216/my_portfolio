3d286d4404303a3338a5ad06b0d33b94362fc390
junji oishi <junji.oishi@nijibox.co.jp>
2023-05-31 17:06:46 +0900

    my_portfolio
    カルーセルを画像に変更

--- src/hooks/useCarousel.tsx
+++ src/hooks/useCarousel.tsx
@@ -2,11 +2,17 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import portfolio_home from "../assets/portfolio_home_slide.png";
 import portfolio_skills from "../assets/portfolio_skills_slide.png";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 const carouselItems = [
     { image: portfolio_home, alt: "" },
     { image: portfolio_skills, alt: "" },
 ]
+
+const imageWidth = css`
+  width: 100%;
+`
 
 export const Carousel = () => {
 
@@ -23,24 +29,15 @@ export const Carousel = () => {
 
     return (
         <Slider {...sliderSettings}>
-            <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          {carouselItems.map((carouselItem, index) => (
+            <div key={index}>
+              <img
+                css={imageWidth}
+                src={carouselItem.image}
+                alt={carouselItem.alt}
+              />
+            </div>
+          ))}
         </Slider>
     );
 };
